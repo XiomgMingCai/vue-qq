96ce0c6e78d24c72c1772f938e7a60f2192aaf14
xiongMingCai <xmc000@icloud.com>
2018-01-13 10:49:33 +0800

    ftp 上传

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var ftp = require('gulp-ftp');
+
+
+
+gulp.task('default', function () {
+    return gulp.src('server/test/*')
+        .pipe(ftp(require('/Users/xiongMingCai/.password').ftp))
+        .pipe(gutil.noop());
+});
